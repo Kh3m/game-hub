3224ea29545ee01f2ba8ab898408aa72aaa3eb76
unknown <khem6333@gmail.com>
2023-10-10 22:25:49 +0100

    Processed and Open

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,36 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from 'axios';
 
-export default axios.create({
-  baseURL: "https://api.rawg.io/api",
+export interface FetchResponse<T> {
+  count: number;
+  next: string | null;
+  results: T[];
+}
+
+const axiosInstance = axios.create({
+  baseURL: 'https://api.rawg.io/api',
   params: {
-    key: "24c4531d012c4e36b36040f982dce5b5"
-  }
+     key: "24c4531d012c4e36b36040f982dce5b5",
+  },
 });
+
+class APIClient<T> {
+  endpoint: string;
+
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
+  }
+
+  getAll = (config: AxiosRequestConfig) => {
+    return axiosInstance
+      .get<FetchResponse<T>>(this.endpoint, config)
+      .then((res) => res.data);
+  };
+
+  get = (id: number | string) => {
+    return axiosInstance
+      .get<T>(this.endpoint + '/' + id)
+      .then((res) => res.data);
+  };
+}
+
+export default APIClient;
